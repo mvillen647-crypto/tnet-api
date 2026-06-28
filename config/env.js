0d119928e9d2533891be7ed20ec32205a1d77554
mvillen647-crypto <mvillen647@gmail.com>
2026-06-28 12:39:33 +0300

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,1 +1,8 @@
+const env = {
+  NODE_ENV: process.env.NODE_ENV || "development",
 
+  SIGHTENGINE_API_USER: process.env.SIGHTENGINE_API_USER || "",
+  SIGHTENGINE_API_SECRET: process.env.SIGHTENGINE_API_SECRET || ""
+};
+
+export default env;
